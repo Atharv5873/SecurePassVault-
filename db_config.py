6421322c2ee2a6000eb51c3c2b42278151c083d3
Atharv Sharma <atharv5873@gmail.com>
2025-06-05 22:00:50 +0530

    Externalized MongoDB URI to config file and added to .gitignore

--- db_config.py
+++ db_config.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
-clinet=MongoClient("mongodb://localhost:27017/")
-db=clinet["vault_db"]
-vault_collection=db["collection"]
+from config import MONGODB_URI
+
+client = MongoClient(MONGODB_URI)
+db = client["vault_db"]
+vault_collection = db["collection"]
