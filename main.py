042893621b0a4686abd4279baf36dce415d4c69b
Atharv Sharma <atharv5873@gmail.com>
2025-06-05 21:40:26 +0530

    Added CORS middleware support for frontend access

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from routers import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="SecurePassVault API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow React or any domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
