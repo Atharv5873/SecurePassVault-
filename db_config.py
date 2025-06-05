92b4f3fa048e5e53bf1acbd3975d2b4436d07c75
Atharv Sharma <atharv5873@gmail.com>
2025-06-05 22:26:04 +0530

    tweeking

--- db_config.py
+++ db_config.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from config import MONGODB_URI
 
-client = MongoClient(MONGODB_URI)
+client = MongoClient("mongodb://vault_user:atharv5873@localhost:27017/vault_db")
 db = client["vault_db"]
 vault_collection = db["collection"]
