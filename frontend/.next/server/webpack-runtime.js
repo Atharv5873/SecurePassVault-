ad1dbb3788c6576f0faa56992a6c5a75bc60f231
Vatanesh Sharma <vataneshsharma@gmail.com>
2025-07-03 16:40:46 +0530

    auto-log out on session expire

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9aeb2bd2b9a0e725")
+/******/ 		__webpack_require__.h = () => ("415998cb40c659e0")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
