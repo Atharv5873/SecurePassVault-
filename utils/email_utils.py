efbff845d72146c1b684385c6f7b54118653ddc3
Vatanesh Sharma <vataneshsharma@gmail.com>
2025-11-07 12:54:24 +0530

    migrated to mailgun

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -1,17 +1,24 @@
-import smtplib
-from dotenv import load_dotenv
-from email.message import EmailMessage
 import os
+import requests
+from dotenv import load_dotenv
 
-load_dotenv() 
+load_dotenv()
 
 def send_otp_email(to_email: str, otp: str):
-    msg = EmailMessage()
-    msg.set_content(f"Your SecurePassVault registration OTP is: {otp}")
-    msg["Subject"] = "Verify your email - SecurePassVault"
-    msg["From"] = os.getenv("EMAIL_SENDER")  
-    msg["To"] = to_email
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(os.getenv("EMAIL_SENDER"), os.getenv("EMAIL_PASSWORD"))
-        smtp.send_message(msg)
+    MAILGUN_DOMAIN = os.getenv("MAILGUN_DOMAIN")
+    MAILGUN_API_KEY = os.getenv("MAILGUN_API_KEY")
+    EMAIL_SENDER = os.getenv("EMAIL_SENDER")
+    
+    if not all([MAILGUN_DOMAIN, MAILGUN_API_KEY, EMAIL_SENDER]):
+        raise ValueError("Mailgun API key, domain, or sender email is not set in environment variables")
+    
+    return requests.post(
+        f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
+        auth=("api", MAILGUN_API_KEY),
+        data={
+            "from": EMAIL_SENDER,
+            "to": to_email,
+            "subject": "Verify your email - SecurePassVault",
+            "text": f"Your SecurePassVault registration OTP is: {otp}"
+        }
+    )
